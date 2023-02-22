40c43ff84036ec0a869f02c6441ed1ab825d3db0
Arijit-NRI <123269125+Arijit-NRI@users.noreply.github.com>
2023-02-22 21:58:03 +0530

    Add files via upload

---
+++ Frontend/index.js
@@ -0,0 +1,17 @@
+
+/* 
+
+    @author: Arijit Saha
+	@desc: js for index.html (Header & Footer Template)
+	@file path from root: Ex ./index.js/
+
+*/
+
+var nrifintech="NRI TRAVEL";
+var booking = `My Bookings`; 
+var logout="Logout";
+
+document.getElementById("nrifintech").innerHTML=nrifintech;
+document.getElementById("booking").innerHTML = booking; 
+document.getElementById("logout").innerHTML = logout;
+
