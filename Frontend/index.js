85daf6571c771f1a7dc7d9ace6047cbb95b70c1f
Arijit-NRI <123269125+Arijit-NRI@users.noreply.github.com>
2023-02-27 00:44:42 +0530

    Add files via upload

---
+++ Frontend/index.js
@@ -0,0 +1,68 @@
+
+/* 
+
+    @author: Arijit Saha
+	@desc: js for index.html (Header & Footer Template)
+	@file path from root: Ex ./index.js/
+
+*/
+
+var nrifintech="NRI TRAVEL";
+var booking = `My Bookings`; 
+var logout="Logout";
+var home="Home";
+
+// document.getElementById("nrifintech").innerHTML=nrifintech;
+// document.getElementById("booking").innerHTML = booking; 
+// document.getElementById("logout").innerHTML = logout;
+
+document.querySelector(".nrifintech").innerHTML=nrifintech;
+document.querySelector(".booking").innerHTML=booking;
+document.querySelector(".logout").innerHTML=logout;
+// document.querySelector(".home").innerHTML=home;
+
+function togglepassword() {
+	var x = document.getElementById("mypassword");
+	var y=document.getElementById("mypassword-icon");
+	if (x.type === "password") {
+		
+	  	
+		y.src="./public/hide-eye.png";
+	  x.type = "text";
+	} else {
+	  x.type = "password";
+	  y.src="./public/eye-icon.svg";
+	  y.class="hidden-eye";
+	}
+}
+
+function on() {
+	// document.getElementById("overlay").style.display = "block";
+	document.querySelector(".cancel-overlay").style.display="block";
+}
+  
+function off() {
+	// document.getElementById("overlay").style.display = "none";
+	document.querySelector(".cancel-overlay").style.display="none";
+}
+
+function onroute()
+{
+	document.querySelector(".route-overlay").style.display="block";
+}
+function offroute()
+{
+	document.querySelector(".route-overlay").style.display="none";
+}
+function homeonroute()
+{
+	document.querySelector(".home-route-overlay").style.display="block";
+}
+function homeoffroute()
+{
+	document.querySelector(".home-route-overlay").style.display="none";
+}
+
+  
+
+
